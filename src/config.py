31de0d09190a5cf3416d9849fb352f194749ed78
FrankLuis <46803159+ryuukousho@users.noreply.github.com>
2025-12-16 08:59:23 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ parser.add_argument("--dataset", "--d", type=str, default="DEBUG",
                     help="Knowledge Graph dataset")
 parser.add_argument("--model", choices=['CogRMH'], default="CogRMH",
                     help="Knowledge Graph embedding models")
-parser.add_argument("--c-init", default=1.0, type=int,
+parser.add_argument("--c-init", default=1.0, type=float,
                     help="Initialized absolute curvature")
 parser.add_argument("--debug", action="store_true",
                     help="Only use 1000 examples for debugging")
